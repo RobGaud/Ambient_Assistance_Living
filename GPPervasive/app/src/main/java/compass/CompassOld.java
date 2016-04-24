66ed108c16f4563513c466165b38d41d9108f958
RobGaud <robgaudenzi@gmail.com>
2016-04-24 12:47:04 +0200

    progress in navigation

--- GPPervasive/app/src/main/java/compass/CompassOld.java
+++ GPPervasive/app/src/main/java/compass/CompassOld.java
@@ -1,10 +1,12 @@
 package compass;
 
+import android.app.Activity;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.renderscript.Float2;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
@@ -12,25 +14,46 @@ import android.widget.ImageView;
 import android.content.Context;
 import android.widget.TextView;
 
+import java.util.ListIterator;
+
+import graph.Edge;
+import graph.Node;
+import ui.Navigation;
+
 /**
  * Created by Andrea on 14/04/2016.
  */
 public class CompassOld  implements SensorEventListener  {
+
+    private static final String TAG_DEBUG = "CompassOld";
     // device sensor manager
     private SensorManager mSensorManager;
+
+    private Navigation activity;
     private ImageView iv;
     private TextView tv=null;
+
     private boolean follow=false;
     private float direction;
     private float range;
+    private Node currentNode;
+
+    public float getCurrentDegree() {
+        return currentDegree;
+    }
+
+    public float getRange() {
+        return range;
+    }
 
     // record the compass picture angle turned
     private float currentDegree = 0f;
 
     //initialize the class
-    public CompassOld(Context context, ImageView i, TextView tv){
+    public CompassOld(Context context, Navigation n, ImageView i, TextView tv){
         // initialize your android device sensor capabilities
         mSensorManager = (SensorManager)context.getSystemService(context.SENSOR_SERVICE);
+        this.activity = n;
         this.tv = tv;
         iv = i;
     }
@@ -76,25 +99,20 @@ public class CompassOld  implements SensorEventListener  {
         //it means that we have to check that the current degree doesn't exceed
         //a certain range
 
-        // 358-1
-        // 5-1
+        // Check user's direction
         float difference = degree-direction;
-        if(follow){
-            String msg;
-            if(difference>0){
-                msg = "sei a dx della direction";
-            }else if(difference<0){
-                msg = "sei a sx della direction";
-            }else{
-                msg = "sei giusto";
-            }
-            //tv.append("\ndirection: "+direction+"\nmsg: "+msg);
-        }
+        this.adjustDirection(difference);
 
         // Start the animation
         iv.startAnimation(ra);
         currentDegree = -degree;
+
+        //Check whether the user has something in front of him
+        if( currentNode != null ){
+            this.checkDirection();
+        }
     }
+
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
         // not in use
@@ -106,4 +124,41 @@ public class CompassOld  implements SensorEventListener  {
     }
 
     private void unsetDirection(){follow=false;}
+
+    public void setCurrentNode(Node n){ this.currentNode = n; }
+
+    // Metodo per vedere cosa ha di fronte (quando non siamo in navigazione)
+    public void checkDirection(){
+        ListIterator<Edge> directions = currentNode.getEdges().listIterator();
+        while( directions.hasNext() ) {
+
+            Edge e = directions.next();
+            float direction = e.getDirection();
+            float currentDegree = this.currentDegree;
+            float range = this.range;
+
+            if (currentDegree >= direction - range && currentDegree <= direction + range) {
+                //TODO interagisci con la Navigation
+                Node possibleDestination = e.getNodeTo();
+                activity.changeStatus(possibleDestination);
+            }
+        }
+    }
+
+    // Metodo per correggere la direzione dell'utente (in navigazione)
+    public void adjustDirection(float difference){
+        //TODO metti la modifica che avevi fatto.
+        if(follow){
+            String msg;
+            if(difference>0){
+                msg = "sei a dx della direction";
+            }else if(difference<0){
+                msg = "sei a sx della direction";
+            }else{
+                msg = "sei giusto";
+            }
+            //tv.append("\ndirection: "+direction+"\nmsg: "+msg);
+        }
+    }
+
 }
--- GPPervasive/app/src/main/java/ui/Navigation.java
+++ GPPervasive/app/src/main/java/ui/Navigation.java
@@ -1,5 +1,6 @@
 package ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,10 +16,13 @@ import com.pervasivesystems.compasstest.R;
 
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.ListIterator;
 
 import compass.CompassOld;
+import graph.Edge;
 import graph.GraphMap;
 import graph.Node;
+import service.BeaconService;
 
 public class Navigation extends AppCompatActivity {
 
@@ -37,7 +41,8 @@ public class Navigation extends AppCompatActivity {
     private static LinkedHashMap<Region, Node> DIAGList;
     private static GraphMap graph;
 
-    private CompassOld compass = new CompassOld(this.getApplicationContext(), compassImage, null);
+    private CompassOld compass = new CompassOld(this.getApplicationContext(),
+                                                this, compassImage, null);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +53,12 @@ public class Navigation extends AppCompatActivity {
         currentNode = null;     // Forse ce lo facciamo passare dalla precedente Activity?
         currentDirection = null;
 
+        String UUID = getIntent().getExtras().getString("Region");
+        if(UUID != null){
+            Log.d(TAG_DEBUG,"UUID passato: "+UUID);
+        }else{
+            Log.d(TAG_DEBUG,"Errore, parametro non passato");
+        }
 
         DIAGList = new LinkedHashMap();
         final GraphMap graph = new GraphMap(DIAGList);
@@ -68,9 +79,50 @@ public class Navigation extends AppCompatActivity {
                     if(currentNode==null) {
                         Log.d(TAG_DEBUG, "ERROR: DETECTED BEACON IS NOT ASSOCIATED TO ANY NODE");
                     }
-                    //TODO
+                    /*  TODO
+                        Interagisci col Compass per sapere se di fronte ha qualcosa
+                    */
+                    compass.setCurrentNode(currentNode);
                 }
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        //stoppo il servizio
+        stopService(new Intent(getBaseContext(), BeaconService.class));
+    }
+
+    /*** Spostato in CompassOld ***
+     public void notifyDirection(Node currentNode, Navigation a){
+        if( currentNode == null ){
+            Log.d(TAG_DEBUG, "Ma come, un nodo vuoto?");
+            return;
+        }
+        ListIterator<Edge> directions = currentNode.getEdges().listIterator();
+        while( directions.hasNext() ){
+
+            Edge e = directions.next();
+            float direction = e.getDirection();
+            float currentDegree = compass.getCurrentDegree();
+            float range = compass.getRange();
+
+            if(currentDegree >= direction-range && currentDegree <= direction+range){
+                a.changeStatus();
+
+            }
+            Node possibleDestination = e.getNodeTo();
+
+        }
+    }
+    */
+
+
+
+    public void changeStatus(Node possibleDirection){
+        //TODO
     }
 }
